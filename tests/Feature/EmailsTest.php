96e500f0b041f20df81b38b614d44d0f2494326e
Markley Mendes <markley.mendes@gmail.com>
2024-01-02 20:32:39 -0300

    test: testing an email was send user:x

--- tests/Feature/EmailsTest.php
+++ tests/Feature/EmailsTest.php
@@ -16,3 +16,14 @@ test('an email was send', function () {
   Mail::assertSent(SendingEmail::class);
 });
 
+test('an email was send user:x', function () {
+  Mail::fake();
+
+  $user = User::factory()->create();
+
+  post(route('sending-email', $user))->assertOk();
+
+  Mail::assertSent(SendingEmail::class, function (SendingEmail $email) use ($user) {
+    return $email->hasTo($user->email);
+  });
+});

debdc822017c1cae68094952eb472b0fde41265c
Markley Mendes <markley.mendes@gmail.com>
2024-01-10 20:04:07 -0300

    test: testing check if is being called

--- tests/Feature/MiddlewaresTest.php
+++ tests/Feature/MiddlewaresTest.php
@@ -1,9 +1,11 @@
 <?php
 
+use App\Http\Middleware\SecureRouteMiddleware;
 use App\Models\User;
 
 use function Pest\Laravel\actingAs;
 use function Pest\Laravel\get;
+use function Pest\Laravel\mock;
 
 it('should block a request if the user does not have the following email: email@email.com', function () {
     $userWrong = User::factory()->create(['email' => 'email@sameemail.com']);
@@ -14,4 +16,13 @@ it('should block a request if the user does not have the following email: email@
 
     actingAs($userRight);
     get(route('secure-route'))->assertOk();
+});
+
+test('check if is being called', function () {
+    mock(SecureRouteMiddleware::class)
+        ->shouldReceive('handle')
+        ->atLeast()
+        ->once();
+
+    get(route('secure-route'));
 });

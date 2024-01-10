6f966f8c78a61b5a6563dd9df70fb081093d45e8
Markley Mendes <markley.mendes@gmail.com>
2024-01-10 19:34:49 -0300

    test: it should be able to create produtc via command

---
+++ tests/Feature/CommandsTest.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\Console\Commands\CreateProductCommand;
+use App\Models\User;
+
+use function Pest\Laravel\actingAs;
+use function Pest\Laravel\artisan;
+use function Pest\Laravel\assertDatabaseCount;
+use function Pest\Laravel\assertDatabaseHas;
+
+it('should be able to create produtc via command', function () {
+    $user = User::factory()->create();
+
+    artisan(
+        CreateProductCommand::class,
+        ['title' => 'product 1', 'user' => $user->id]
+    )->assertSuccessful();
+
+    assertDatabaseHas('products', ['title' => 'product 1', 'owner_id' => $user->id]);
+    assertDatabaseCount('products', 1);
+});
